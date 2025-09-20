ce93b027b1b99f85d880d70ea5d22836d294735a
Gadi Hamid <gadihamid@gmail.com>
2025-09-20 21:54:07 +0100

    Fix router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/ui/Layout";
 import Home from "./pages/Home";
 import Categories from "./pages/categories";
@@ -13,21 +13,19 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="categorie/:category" element={<Categories />} />
-          <Route path="soins" element={<Soins />} />
-          <Route path="apropos" element={<APropos />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="categorie/cheveux" element={<CheveuxPage />} />
-          <Route path="produit/gel-douche" element={<GelDouchePage />} />
-          <Route path="produit/soins-visage" element={<SoinsVisagePage />} />
-          <Route path="produit/:id" element={<ProductPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="categorie/:category" element={<Categories />} />
+        <Route path="soins" element={<Soins />} />
+        <Route path="apropos" element={<APropos />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="categorie/cheveux" element={<CheveuxPage />} />
+        <Route path="produit/gel-douche" element={<GelDouchePage />} />
+        <Route path="produit/soins-visage" element={<SoinsVisagePage />} />
+        <Route path="produit/:id" element={<ProductPage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 
+// Crée la racine et rend l'application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* basename = nom du repo pour GitHub Pages */}
+    <BrowserRouter basename="/niall-cosmetique">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
